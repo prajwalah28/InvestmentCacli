c44c8e0e11c6cf1208ba29202ca7d682cdfcac37
shrutiwalokar <shrutiwalokar@gmail.com>
2024-01-13 11:38:45 +0530

    Initialize project using Create React App

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from '../assets/download (2).jpeg'
+import './Header.css'
+export default function Header() {
+  return (
+    <>
+        <div className="logo">
+            <img   src={logo}/>
+        </div>
+        <div className="Heading">
+            Investment Calculator
+        </div>
+    </>
+  )
+}
